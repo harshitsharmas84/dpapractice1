a18d9dcc13e020ef2ff9af5d17398edf55130bab
sharmaharshitnone <harshit.sharma.s84@kalvium.community>
2025-02-01 23:26:08 +0530

    Done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { resolve } = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = 3010;
+
+const config = {
+  apiKey: process.env.API_KEY,
+  serverSecret: process.env.SERVER_SECRET,
+  isKalvian: process.env.IS_KALVIAN === "true",
+};
+
+app.use(express.static("static"));
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve(__dirname, "pages/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(config); // Optional: Log the config for testing
+});
